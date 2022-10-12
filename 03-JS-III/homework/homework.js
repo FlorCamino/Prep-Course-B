495da96504c42a3487fcb80797f7873c5521a61d
Florencia Camino <flopiis.1994@gmail.com>
2022-10-12 10:44:55 -0300

    Homework unit 3 finished

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,31 +8,58 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0;
+    for(i = 1; i <= 10; i++){
+      acumulador = acumulador + i;
+    }
+return acumulador;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+ var pares = [];
+ for (i = 0; i < array.length; i++){
+  var numero = array[i];
+  if (numero % 2 == 0){
+    pares.push(numero);
+  }
+ }
+ return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var arreglo = [];
+  for (i = 0; i < array.length; i++){
+    var numero = array[i];
+    var elevado = Math.pow(numero, 2);
+    arreglo.push(elevado);
+  }
+  return arreglo;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma = 0;
+  for (i = 0; i < array.length; i++){
+    var result = array[i];
+    var suma = suma + result;
+  } 
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var result = num.toString().length;
+  return result;
 }
-  
   
   // No modificar nada debajo de esta línea
   // --------------------------------
